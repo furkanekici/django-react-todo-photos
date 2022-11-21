f39529a4ddab19173c3569451691a959c5ce5164
Furkan Ekici <furkanekicidev@gmail.com>
2022-11-21 22:29:42 +0300

    get a single photo

--- backend/photos/urls.py
+++ backend/photos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PhotoList
+from .views import PhotoList, PhotoDetail
 
 urlpatterns = [
     path("", PhotoList.as_view(), name="photo_list"),
+    path("<int:pk>/", PhotoDetail.as_view(), name="photo_detail"),
 ]
--- backend/photos/views.py
+++ backend/photos/views.py
@@ -23,3 +23,31 @@ class PhotoList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PhotoDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Photo.objects.get(pk=pk)
+        except Photo.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        photo = self.get_object(pk)
+        serializer = PhotoSerializer(photo)
+        return Response(serializer.data)
+
+    # update a photo
+    def put(self, request, pk, format=None):
+        photo = self.get_object(pk)
+        serializer = PhotoSerializer(photo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # delete a photo
+    def delete(self, request, pk, format=None):
+        photo = self.get_object(pk)
+        photo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
